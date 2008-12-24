5ffdfe31619cff440cdc9fecf9d2e88c47a87040
Riyad Preukschas <riyad@barut.(none)>
2008-12-24 16:03:04 +0100

    Fixed spaces.

--- prayertimes.h
+++ prayertimes.h
@@ -12,21 +12,19 @@ class QSizeF;
 // Define our plasma Applet
 class PrayerTimes : public Plasma::Applet
 {
-    Q_OBJECT
-    public:
-        // Basic Create/Destroy
-        PrayerTimes(QObject *parent, const QVariantList &args);
-        ~PrayerTimes();
+	Q_OBJECT
+public:
+		// Basic Create/Destroy
+		PrayerTimes(QObject *parent, const QVariantList &args);
+		~PrayerTimes();
 
-        // The paintInterface procedure paints the applet to screen
-        void paintInterface(QPainter *painter,
-                const QStyleOptionGraphicsItem *option,
-                const QRect& contentsRect);
-        void init();
+		// The paintInterface procedure paints the applet to screen
+		void paintInterface(QPainter *painter, const QStyleOptionGraphicsItem *option, const QRect& contentsRect);
+		void init();
 
-    private:
-        Plasma::Svg m_svg;
-        KIcon m_icon;
+private:
+		Plasma::Svg m_svg;
+		KIcon m_icon;
 };
  
 // This is the command that links your applet to the .desktop file
