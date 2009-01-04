1a71be1da20045d376411e9eabf47fb055b089ad
Riyad <includeme@users.sourceforge.net>
2009-01-04 05:02:09 +0100

    Added qibla widget.

---
+++ QiblaGraphicsWidget.cpp
@@ -0,0 +1,45 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "QiblaGraphicsWidget.h"
+
+#include <QPainter>
+#include <QStyleOptionGraphicsItem>
+#include <QWidget>
+
+#include <Plasma/Svg>
+
+QiblaGraphicsWidget::QiblaGraphicsWidget(QGraphicsItem* parent, Qt::WindowFlags wFlags)
+	: QGraphicsWidget(parent, wFlags),
+	m_prayertimesSvg(0)
+{
+	m_prayertimesSvg = new Plasma::Svg(this);
+	m_prayertimesSvg->setImagePath("widgets/prayertimes");
+	m_prayertimesSvg->setContainsMultipleImages(true);
+}
+
+void QiblaGraphicsWidget::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+  QGraphicsWidget::paint(painter, option, widget);
+
+	int compassSize = qMin(size().width(), size().height());
+	m_prayertimesSvg->resize(compassSize, compassSize);
+	painter->drawPixmap(0, 0, m_prayertimesSvg->pixmap("compass"));
+
+	painter->drawPixmap(0, 0, m_prayertimesSvg->pixmap("compass_needle"));
+}
+
+#include "QiblaGraphicsWidget.moc"
---
+++ QiblaGraphicsWidget.h
@@ -0,0 +1,42 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef QIBLAGRAPHICSWIDGET_H
+#define QIBLAGRAPHICSWIDGET_H
+
+#include <QGraphicsWidget>
+
+class QPainter;
+class QWidget;
+class QStyleOptionGraphicsItem;
+
+namespace Plasma {
+	class Svg;
+}
+
+class QiblaGraphicsWidget : public QGraphicsWidget
+{
+	Q_OBJECT
+
+	public:
+		QiblaGraphicsWidget(QGraphicsItem *parent=0, Qt::WindowFlags wFlags=0);
+		virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+
+	private:
+		Plasma::Svg *m_prayertimesSvg;
+};
+
+#endif // QIBLAGRAPHICSWIDGET_H
