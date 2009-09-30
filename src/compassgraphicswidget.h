6863c9bbcf34692792f45465feaf90168fddbdd2
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-09-30 21:34:00 +0200

    Excessively documented CompassGraphicsWidget.

--- src/compassgraphicswidget.h
+++ src/compassgraphicswidget.h
@@ -13,6 +13,10 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+/**
+ * @file
+ * Contains a graphis widget showing a themable compass with a movable needle.
+ */
 
 #ifndef QIBLAGRAPHICSWIDGET_H
 #define QIBLAGRAPHICSWIDGET_H
@@ -25,22 +29,51 @@ namespace Plasma {
 	class Svg;
 }
 
+/**
+ * @class CompassGraphicsWidget
+ * A graphis widget showing a themable compass.
+ * The needle can be set to show into the desired direction.
+ */
 class CompassGraphicsWidget : public QGraphicsWidget
 {
 	Q_OBJECT
 
+	//! The direction the needle shoud be pointed to in degrees from north.
 	double m_needle;
 
+	//! The compass' SVG
 	Plasma::Svg *m_compassSvg;
 
-	public:
-		CompassGraphicsWidget(QGraphicsItem *parent=0, Qt::WindowFlags wFlags=0);
+public:
+	CompassGraphicsWidget(QGraphicsItem *parent=0, Qt::WindowFlags wFlags=0);
 
-		void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget=0);
+	/**
+	 * Paints the layers of the compass SVG and orients the needle.
+	 * @param painter The painter to use.
+	 * @param option The style options to use.
+	 * @param widget ???
+	 */
+	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget=0);
 
-		double needle() const;
-		const QString& needleOrientation() const;
-		void setNeedle(const double degrees);
+	/**
+		* Gives you the current needle direction.
+		* @return The needle direction.
+		*/
+	double needle() const;
+
+	/**
+		* Gives you the current needle direction as a compass orientation (N for 0¡Æ or SE for 139¡Æ).
+		* It support a 16 segment resolution (i.e. at most 3 characters like SSE or WNW).
+		* North is defined to be from -11.25¡Æ to +11.25¡Æ (mod 360¡Æ).
+		* @return The orientation.
+		*/
+	const QString& needleOrientation() const;
+
+	/**
+		* Sets the direction the needle shoud be pointed to in degrees from north.
+		* @param degrees The direction.
+		*/
+	void setNeedle(const double degrees);
 };
 
 #endif // QIBLAGRAPHICSWIDGET_H
